a2159f3f491460b23f513351b8ff60fc6f74c701
Z User <z@container>
2026-05-21 12:23:11 +0000

    62bd43de-f1ba-43e3-a91f-6ed60edb0c45

--- start-server.sh
+++ start-server.sh
@@ -2,7 +2,10 @@
 cd /home/z/my-project
 while true; do
   echo "[$(date)] Starting Next.js server..."
-  node .next/standalone/server.js -p 3000 2>&1
+  setsid next start -p 3000 0</dev/null >> /tmp/nextjs.log 2>&1 &
+  SERVER_PID=$!
+  echo "[$(date)] Server PID: $SERVER_PID"
+  wait $SERVER_PID
   EXIT_CODE=$?
   echo "[$(date)] Server exited with code $EXIT_CODE, restarting in 3s..."
   sleep 3
